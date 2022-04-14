8cd043f943ff264f8b7388925ed1941510b4acaa
VampireViktor20 <kylecornw3ll@outlook.com>
2022-04-14 23:49:36 +0100

    Player Camera and Movement Update
    - Movement updated with camera control
    - Toggle for aim mode, need to fix controls in aim mode
    - Working on Wall mechanics

---
+++ Indie Game Prototype/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField] private CinemachineFreeLook thirdPersonCam;
+
+    [SerializeField] private CinemachineVirtualCamera aimCam;
+
+    [SerializeField] private bool aimMode;
+
+
+    public GameObject aimCrossHair;
+
+    void Start()
+    {
+        aimCam.enabled = aimMode;
+        thirdPersonCam.enabled = !aimMode;
+        
+    }
+
+    void Update()
+    {
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            aimMode = !aimMode;
+            thirdPersonCam.enabled = !aimMode;
+            aimCam.enabled = aimMode;
+
+        }
+
+        if (aimMode == true)
+        {
+            aimCrossHair.SetActive(true);
+        }
+        else if(aimMode == false)
+        {
+            aimCrossHair.SetActive(false);
+        }
+        
+    }
+}
